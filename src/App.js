29b15194593241bef6314c63fbcdc0a7c4f37c00
souravdutta029 <duttasourav029@gmail.com>
2020-06-24 08:09:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Tesseract from "tesseract.js";
+import "./App.css";
+
+Tesseract.recognize(
+  "https://tesseract.projectnaptha.com/img/eng_bw.png",
+  "eng",
+  { logger: (m) => console.log(m) }
+).then(({ data: { text } }) => {
+  console.log(text);
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello from React</h1>
     </div>
   );
 }
